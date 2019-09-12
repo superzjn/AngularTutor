8bd3d0e99ea0a1d22b954bff733a1658eeee6318
Nan Zhang <nan.zhang@morningstar.com>
2019-09-12 12:18:41 -0500

    Homework 2.2

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,8 +9,12 @@ import {Student} from '../../shared/student.model';
 export class ServerComponent {
   meat: string;
   vegetable: string;
-  materials: string[] = ['egg', 'tomato', 'onion', 'beef', 'pork', 'garlic', 'cucumber', 'lamb'];
+  dishName: string;
+  materials_meat: string[] = ['egg', 'beef', 'pork', 'lamb'];
+  materials_vege: string[] = ['tomato', 'onion', 'garlic', 'cucumber'];
+
   isValid = false;
+  isNotValid = false;
   counter = 0;
   isLogin = false;
   userName: string;
@@ -18,7 +22,61 @@ export class ServerComponent {
     new Student('Steve Jobs', 'Computer Science'),
     new Student('Elon Musk', 'Computer Science')];
 
-  validate() {
+  cook() {
+    if ((this.materials_meat.indexOf(this.meat) === -1 || this.materials_vege.indexOf(this.vegetable) === -1)) {
+      this.isNotValid = true;
+    } else {
+      this.generateDish();
+      this.isValid = true;
+    }
+  }
+
+  generateDish() {
+    if (this.meat === 'pork') {
+      switch (this.vegetable) {
+        case 'garlic': {
+          this.dishName = 'Garlic Good Pork';
+          console.log(this.dishName);
+          break;
+        }
+        case 'Onion': {
+          this.dishName = 'Onion Fried with Pork';
+          break;
+        }
+        default: {
+          this.dishName = 'Dark Dish';
+          break;
+        }
+      }
+    } else if (this.meat === 'beef') {
+      switch (this.vegetable) {
+        case 'garlic': {
+          this.dishName = 'Garlic Good Beef';
+          break;
+        }
+        case 'onion': {
+          this.dishName = 'Onion Fried with Beef';
+          break;
+        }
+        case 'tomato': {
+          this.dishName = 'Tomato Beef';
+          break;
+        }
+        default: {
+          this.dishName = 'Dark Dish';
+          break;
+        }
+      }
+    } else {
+      this.dishName = 'Dark Dark Dish';
+    }
+  }
+
+  reset() {
+    this.meat = '';
+    this.vegetable = '';
+    this.isNotValid = false;
+    this.isValid = false;
   }
 
   counterPlus() {
